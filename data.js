ac61e12a9cd917175054520bf6207b72cf27b3b1
luciafonsecadesign <luciafonsecadesign@gmail.com>
2024-10-30 05:35:33 -0500

    fix imagen

--- data.js
+++ data.js
@@ -1,51 +1,7 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-sala",
-      "name": "Sala",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": -0.1919862177193803,
-        "pitch": 0.20044007629787686,
-        "fov": 1.4413225997054397
-      },
-      "linkHotspots": [
-        {
-          "yaw": -0.3581140955194204,
-          "pitch": 0.051789707461471934,
-          "rotation": 0,
-          "target": "2-bao"
-        },
-        {
-          "yaw": -3.0385757181536572,
-          "pitch": -0.03786380116296684,
-          "rotation": 0,
-          "target": "1-cama"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "1-cama",
+      "id": "0-cama",
       "name": "Cama",
       "levels": [
         {
@@ -68,16 +24,60 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": 0.04161938985525104,
-        "pitch": 0.3446939633455237,
+        "yaw": -2.2686230164482897,
+        "pitch": 0.2980218841528206,
         "fov": 1.4413225997054397
       },
       "linkHotspots": [
         {
-          "yaw": 1.3151735590680982,
-          "pitch": 0.04674471604346664,
+          "yaw": -1.1690655876918594,
+          "pitch": 0.1941591936557181,
           "rotation": 0,
-          "target": "0-sala"
+          "target": "1-sala"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-sala",
+      "name": "Sala",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.2873724312342407,
+        "pitch": 0.08928547077821491,
+        "fov": 1.4413225997054397
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.31750457125622056,
+          "pitch": 0.08281330341323212,
+          "rotation": 0,
+          "target": "2-bao"
+        },
+        {
+          "yaw": -3.0980570981304396,
+          "pitch": 0.15514485794596844,
+          "rotation": 0,
+          "target": "0-cama"
         }
       ],
       "infoHotspots": []
@@ -106,22 +106,22 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": -0.252026702682576,
-        "pitch": 0.3991360361502796,
+        "yaw": -0.004027682889217843,
+        "pitch": 0.3407425109290969,
         "fov": 1.4413225997054397
       },
       "linkHotspots": [
         {
-          "yaw": -2.735369746858007,
-          "pitch": 0.1327565372525612,
+          "yaw": -2.7197739765866356,
+          "pitch": 0.00858166848760078,
           "rotation": 0,
-          "target": "0-sala"
+          "target": "1-sala"
         }
       ],
       "infoHotspots": []
     }
   ],
-  "name": "Ryo suite 2",
+  "name": "Ryo Suite 2",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
